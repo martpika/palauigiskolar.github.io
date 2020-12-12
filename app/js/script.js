a6f987de30287c547493858f67cbfaac26177668
pikamart <pamplona.raymart02@gmail.com>
2020-12-11 20:12:28 -0800

    40%

--- app/js/script.js
+++ app/js/script.js
@@ -6,3 +6,42 @@ function slidemenu(){
     x.className = "navigation-links";
   }
 }
+
+// Section for Image Slider
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+const prevBtn = document.querySelector("#prevbtn");
+const nextBtn = document.querySelector("#nextbtn");
+let counter = 1;
+let size = carouselImages[0].clientWidth;
+
+// Start from the second image
+carouselSlide.style.transform = "translateX(" + (-size*counter) + "px)"; 
+// Button event listeners
+nextBtn.addEventListener("click", ()=>{
+    if (counter >= carouselImages.length-1) return;
+    carouselSlide.style.transition = "transform .4s ease-in-out";
+    size = carouselImages[0].clientWidth;
+    counter++;
+    carouselSlide.style.transform = "translateX(" + (-size*counter) + "px)"; 
+});
+prevBtn.addEventListener("click", ()=>{
+    if (counter <= 0) return;
+    carouselSlide.style.transition = "transform .4s ease-in-out";
+    size = carouselImages[0].clientWidth;
+    counter--;
+    carouselSlide.style.transform = "translateX(" + (-size*counter) + "px)"; 
+});
+// Add ending event listener
+carouselSlide.addEventListener("transitionend", ()=>{
+  if (carouselImages[counter].id === "lastclone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length-2;
+    carouselSlide.style.transform = "translateX(" + (-size*counter) + "px)"; 
+  }
+  if (carouselImages[counter].id === "firstclone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length-counter;
+    carouselSlide.style.transform = "translateX(" + (-size*counter) + "px)"; 
+  }
+});
